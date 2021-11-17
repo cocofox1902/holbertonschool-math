069c600d427e76f725829e199e4e06295db885d5
cocofox1902 <colas.renard1902@gmail.com>
2021-11-17 17:59:41 +0000

    library v0

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+typedef struct complex
+{
+	double re;
+	double im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif /* HOLBERTON_H */
